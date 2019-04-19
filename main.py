e82a3e34aac3aecda226ed8906f9ccb5c7ff6de2
Jorge Pereira <JorgePe@users.noreply.github.com>
2019-04-19 10:14:34 +0100

    Update main.py
    Use hostname as MQTT Client ID instead of defining on the code.
    Add a few comments to pretend I am a good programmer.

--- main.py
+++ main.py
@@ -10,16 +10,35 @@ from pybricks.robotics import DriveBase
 
 from umqtt.robust import MQTTClient
 import time
+import os
 
 motor = Motor(Port.A)
 
-MQTT_ClientID = 'Alpha'
-#MQTT_ClientID = 'Iota'
+########################################################################
+# get hostname to use as Client ID
+# assuming each EV3 brick had it's hostname changed after installation
+########################################################################
+os.system('hostname > hostname.txt')
+file = open('hostname.txt', 'r')
+MQTT_ClientID = file.readline().rstrip('\n')
+file.close()
+os.system('rm hostname.txt')
+
+########################################################################
+# There are public brokers available but you we can also install
+# 'mosquitto' and run our own private broker
+# note: ev3dev image already has 'mosquitto' instaled, we can start the
+# broker with 'sudo service mosquitto start'
+########################################################################
 #MQTT_Broker = 'test.mosquitto.org'
 MQTT_Broker = '10.24.10.121'
+
+# Define a namespace for all messages
 MQTT_Topic_Status = 'JorgePe/Status'
 MQTT_Topic_Motor = 'JorgePe/Motor'
 
+# callback message to process any new message appearing at the subscribed
+# topics
 def getmessages(topic, msg):
     if topic == MQTT_Topic_Status.encode():
         brick.display.text(str(msg.decode()))
